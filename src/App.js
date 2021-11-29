90154bbbaf12dffe0b9c6d47a5e46b89ae04d32d
thana.wisutphusi <thana.wisutphusi@thoughtworks.com>
2021-11-29 21:11:23 +0700

    add eslint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+class App extends React.Component {
+
+  render() {
   return (
     <div className="App">
       <header className="App-header">
@@ -17,9 +20,13 @@ function App() {
         >
           Learn React
         </a>
+        <h1>
+          asd
+        </h1>
       </header>
     </div>
   );
+  }
 }
 
 export default App;
